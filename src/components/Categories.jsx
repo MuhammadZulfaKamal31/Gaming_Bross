a16103461a0c0eb879f7bdc0a1c7274df78302ff
iqbalxdrk <iqbalherdiansyah204@gmail.com>
2023-03-30 17:51:07 +0700

    modified categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,72 +1,38 @@
 import React from "react";
+import items from "../categories.json";
 
 const Categories = () => {
-  return (
-    <>
-      <div className="px-36 py-16">
-        <h1 className="font-bold text-[30px] text-black">
-          Browse By Categories
-        </h1>
-        <div className="grid grid-flow-col grid-cols-6 gap-10 mt-6">
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-psconsole.jpg"
-              alt="Playstation"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Playstation</p>
-          </div>
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-xboxconsole.jpg"
-              alt="Xbox"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Xbox One Console</p>
-          </div>
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-switchaccessories.jpg"
-              alt="Playstation"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Nintendo Switch Console</p>
-          </div>
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-psaccessories.jpg"
-              alt="Playstation"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Playstation Accessories</p>
-          </div>
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-nintendogame.jpg"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Nitendo Switch Games</p>
-          </div>
-          <div className="">
-            <img
-              src="https://www.psegameshop.com/wp-content/uploads/2021/04/ns-xbox-games.jpg"
-              alt="Playstation"
-              className="rounded-xl"
-            />
-            <p className="text-black font-bold text-center mt-2">Xbox One Game</p>
-          </div>
-        </div>
-        <div className="mt-10 py-10 px-5">
-        <div className="bg-gradient-to-r from-blue-900 to-slate-900 p-14 rounded-2xl text-white">
-            <div className="flex items-center justify-center gap-16">
-            <h1 className="font-bold text-[25px]">Jangan Lupa Mengirimkan Konfirmasi Pembayaran Kepada Kami</h1>
-            <button className="btn text-base">Send Payment Confirmation<span className="m-2"> ❯</span></button>
-            </div>
-        </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="px-36 py-16">
+				<h1 className="font-bold text-[30px] text-black">
+					Browse By Categories
+				</h1>
+				<div className="grid grid-flow-col grid-cols-6 gap-10 mt-6">
+					{items.map(({ image, title }, i) => (
+						<div key={`categories_item_${i}`} className="group">
+							<div className="categories-item">
+								<img src={image} alt={title} className="categories-image" />
+							</div>
+							<p className="text-black font-bold text-center mt-2">{title}</p>
+						</div>
+					))}
+				</div>
+				<div className="mt-10 py-10 px-5">
+					<div className="bg-gradient-to-r from-blue-900 to-slate-900 p-14 rounded-2xl text-white">
+						<div className="flex items-center justify-center gap-16">
+							<h1 className="font-bold text-[25px]">
+								Jangan Lupa Mengirimkan Konfirmasi Pembayaran Kepada Kami
+							</h1>
+							<button className="btn text-base">
+								Send Payment Confirmation<span className="m-2"> ❯</span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Categories;
